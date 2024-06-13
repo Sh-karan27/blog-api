6202dbc9d668f711c664644bdaa5b6ea8e1eea17
Sh-karan27 <shuklakaran107@gmail.com>
2024-06-13 21:08:02 +0530

    cc

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT: ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("ERRR:", error);
+      throw error;
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
