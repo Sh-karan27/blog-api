faeee67543b010a6141a74c487a8f5cb00cc6e2f
Sh-karan27 <shuklakaran107@gmail.com>
2024-06-20 14:30:55 +0530

    added subscription model/controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,7 +7,62 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleFollower = asyncHandler(async (req, res) => {
   const { profileId } = req.params;
-  //toggle follower
+
+  if (!isValidObjectId(profileId)) {
+    throw new ApiError(401, "please enter a valid id");
+  }
+
+  const userProfile = await User.findById(profileId).select(
+    "username profileImage"
+  );
+
+  if (!userProfile) {
+    throw new ApiError(401, "failed to fectch user profile");
+  }
+
+  if (req.user._id.toString() === profileId.toString()) {
+    throw new ApiError(400, "You cannot follow to your own profile.");
+  }
+
+  const follower = await Subscription.findOne({
+    follower: req.user?._id,
+    following: profileId,
+  });
+
+  if (follower) {
+    await Subscription.deleteOne({
+      follower: req.user?._id,
+      following: profileId,
+    });
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { following: false, userProfile },
+          "unfollowed successfully"
+        )
+      );
+  }
+
+  const startedFollowing = await Subscription.create({
+    follower: req.user?._id,
+    following: profileId,
+  });
+
+  if (!startedFollowing) {
+    throw new ApiError(400, "Unable to follow this user please try again");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { following: true, userProfile },
+        "you are now following this user"
+      )
+    );
 });
 
 const getUserProfileFollower = asyncHandler(async (req, res) => {
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema(
+const followerSchema = new Schema(
   {
     follower: {
       type: Schema.Types.ObjectId,
