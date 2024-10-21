c7d5e2db4354fdccbbd151b7372d19fd3ef5cabc
Sh-karan27 <shuklakaran107@gmail.com>
2024-10-21 14:36:45 +0530

    cors origin

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://blog-api-ten-rust.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(
   express.json({
