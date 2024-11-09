48f15d3d64b01367e2ced6fc3c68de1caca3520d
Sh-karan27 <shuklakaran107@gmail.com>
2024-11-09 21:00:25 +0530

    cc

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -20,5 +20,5 @@
 
 
 import multer from "multer";
-
-export const upload = multer({ storage: multer.memoryStorage() });
+const storage = multer.memoryStorage();
+export const upload = multer({ storage: storage });
